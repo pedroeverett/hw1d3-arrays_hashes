e7cbae960a78e1700b8bfce89d54f62fba8a70c7
Pedro Everett <pedrojoseeverett@gmail.com>
2017-05-24 17:23:59 +0100

    Group 3 Question 1

--- homework.rb
+++ homework.rb
@@ -37,17 +37,19 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # # 1. How would you return the string `"One"`?
-# puts my_hash[1]
+# my_hash[1]
 
 # 2. How would you return the string `"Two"`?
-# puts my_hash[:two]
+# my_hash[:two]
 
 # 3. How would you return the number `2`?
-puts my_hash["two"]
+# my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+# my_hash[3] = "Three"
 
 # 5. How would you add `{:four => 4}` to the hash?
+# my_hash[:four] = 4
 
 ### C. Given the following data structure:
 
@@ -85,6 +87,7 @@ users = {
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 
+puts users["Jonathan"][:twitter]
 # 2. Return Erik's hometown
 
 # 3. Return the array of Erik's favorite numbers
