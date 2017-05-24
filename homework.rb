49f98b6f19e7a91edbb24969edc1025d76d239d9
Pedro Everett <pedrojoseeverett@gmail.com>
2017-05-24 17:09:03 +0100

    Group 1 Question 9

--- homework.rb
+++ homework.rb
@@ -2,32 +2,35 @@
 
 lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
-# 1. Work out how many stops there are in the array
-puts lines.length()
+# # 1. Work out how many stops there are in the array
+# puts lines.length()
 
-# 2. Return 'Edinburgh Park' from the array
-puts lines[1]
+# # 2. Return 'Edinburgh Park' from the array
+# puts lines[1]
 
-# 3. How many ways can we return 'Princes Street' from the array?
-puts lines[4]
-puts lines[-1]
-puts lines.last
+# # 3. How many ways can we return 'Princes Street' from the array?
+# puts lines[4]
+# puts lines[-1]
+# puts lines.last
 
-# 4. Work out the index position of 'Haymarket'
-puts lines.index('Haymarket')
+# # 4. Work out the index position of 'Haymarket'
+# puts lines.index('Haymarket')
 
-# 5. Add 'Airport' to the start of the array
-puts lines.unshift("Airport")
+# # 5. Add 'Airport' to the start of the array
+# puts lines.unshift("Airport")
 
-# 6. Add 'York Place' to the end of the array
-puts lines.push("York Place")
+# # 6. Add 'York Place' to the end of the array
+# puts lines.push("York Place")
 
-# 7. Remove 'Edinburgh Park' from the array using it's name
-puts lines.delete("Edinburgh Park")
+# # 7. Remove 'Edinburgh Park' from the array using it's name
+# puts lines.delete("Edinburgh Park")
 
-# 8. Delete 'Edinburgh Park' from the array by index
+# # 8. Delete 'Edinburgh Park' from the array by index
+# lines.delete_at(1)
+# puts lines
 
 # 9. Reverse the positions of the stops in the array
+puts lines.reverse
 
 ### B. Given the following data structure:
 
